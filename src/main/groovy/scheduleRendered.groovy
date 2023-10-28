58de2c57ba059ec3f725377da021e9605230c64f
pvranik <petr.vranik@mgm-tp.com>
2023-10-28 20:45:05 +0200

    TimeTable renderer

--- src/main/groovy/scheduleRendered.groovy
+++ src/main/groovy/scheduleRendered.groovy
@@ -8,6 +8,7 @@ import net.sourceforge.plantuml.FileFormat
 import net.sourceforge.plantuml.FileFormatOption
 import net.sourceforge.plantuml.SourceStringReader
 import net.suteren.netatmo.domain.therm.Schedule
+import net.suteren.netatmo.domain.therm.TimetableEntry
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.slf4j.simple.SimpleLogger
@@ -16,6 +17,7 @@ import picocli.CommandLine
 import java.nio.charset.Charset
 
 @Field final Logger log = LoggerFactory.getLogger('scheduleRenderer')
+@Field public static final int MINUTES_PER_DAY = 1440
 
 @CommandLine.Parameters(arity = "1", paramLabel = "SECHEDULE", description = "Schedule JSON file")
 @Field File scheduleFile
@@ -27,14 +29,35 @@ if (debug) {
 }
 
 @Field final static JSON_MAPPER = JsonMapper.builder().build()
+@Field Map<Integer, String> zoneNames
+@Field Map<Integer, String> zoneColors = [0: "GreenYellow", 1: "MediumSlateBlue", 2: "yellow", 3: "orange", 4: "cyan"]
 
 Schedule schedule = JSON_MAPPER.readValue(scheduleFile, Schedule)
+zoneNames = schedule.zones().collectEntries { [(it.id()): it.name()] }
+List<TimetableEntry> timetable = schedule.timetable()
+def days = timetable.groupBy { it.mOffset() / MINUTES_PER_DAY as int }
 
+String source = """
+@startuml
 
+title Weekly schedule: ${schedule.name()}
+
+concise "Mo" as d0
+concise "Tu" as d1
+concise "We" as d2
+concise "Th" as d3
+concise "Fr" as d4
+concise "Sa" as d5
+concise "Fr" as d6
+
+${initDays(days)}
+
+${renderDays(days)}
+@enduml
+"""
+
+println source
 //======================================================================//
-String source = "@startuml\n";
-source += "Bob -> Alice : hello\n";
-source += "@enduml\n";
 
 SourceStringReader reader = new SourceStringReader(source);
 final ByteArrayOutputStream os = new ByteArrayOutputStream();
@@ -47,3 +70,31 @@ os.close();
 final String svg = new String(os.toByteArray(), Charset.forName("UTF-8"));
 
 log.info(svg)
+
+String renderDays(Map<BigDecimal, List<TimetableEntry>> days) {
+	days.collect { d, t ->
+		"""
+@d${d}
+${renderTimes(t)}
+"""
+	}
+			.join('\n\n')
+}
+
+String renderTimes(List<TimetableEntry> timetableEntries) {
+	timetableEntries.collect { "${offsetToTime(it)} is \"${zoneNames[it.zoneId()]}\" #${zoneColors[it.zoneId()]}" }
+			.join('\n')
+}
+
+private static String offsetToTime(TimetableEntry it) {
+	def minutes = it.mOffset() % MINUTES_PER_DAY
+	"${minutes / 60 as int}:${minutes % 60}:00"
+}
+
+String initDays(Map<Integer, List<TimetableEntry>> days) {
+	days
+			.findAll { it.key < 6 }
+			.collect { d, t -> "d${d + 1} is \"${zoneNames[(t.last().zoneId())]}\" #${zoneColors[t.last().zoneId()]}" }
+			.join('\n')
+
+}
--- src/main/java/net/suteren/netatmo/domain/therm/TimetableEntry.java
+++ src/main/java/net/suteren/netatmo/domain/therm/TimetableEntry.java
@@ -5,6 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(JsonInclude.Include.NON_ABSENT)
 public record TimetableEntry(
-	@JsonProperty("zone_id") String zoneId,
+	@JsonProperty("zone_id") Integer zoneId,
 	@JsonProperty("m_offset") int mOffset
 ) {}
